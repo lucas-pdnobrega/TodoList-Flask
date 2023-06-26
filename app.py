669246bfe2e57d688302511f8d182ebd284f19c4
Lucas Nobrega <lucaspdn04@gmail.com>
2023-06-26 10:23:14 -0300

    Databank initialization, fixing POST, GET

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, request
 from flask_restful import Resource, Api
 from secure_check import authenticate, identity
 from flask_jwt import JWT, jwt_required
@@ -16,6 +17,11 @@ Migrate(app, db)
 api = Api(app)
 #jwt = JWT(app, authenticate, identity)
 
+#parser = reqparse.RequestParser()
+#parser.add_argument('title', type=str)
+#parser.add_argument('description', type=str, required=False)
+#parser.add_argument('due', type=str, required=False)
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
@@ -31,27 +37,56 @@ class Task(db.Model):
         return {
                 'title' : self.title,
                 'description' : self.description,
-                'due' : self.due
+                'due' : self.due.isoformat()
                 }
 
 class TodoList(Resource):
     
     #@jwt_required()
-    def get(self, title):
+    def get(self):
+
+        title = request.args.get('title')
+
         todoList = Task.query.filter_by(title=title)
+
         if todoList:
             return [todo.json() for todo in todoList]
         return {'title': None}, 404
     
     #@jwt_required()
-    def post(self, title, description, due):
-        todo = Task(title=title, description=description, due=due)
+    def post(self):
+
+        title = request.args.get('title')
+        description = request.args.get('description')
+        due = request.args.get('due')
+
+        todo = Task(title=title,
+                    description=description.replace("_", " "),
+                    due=datetime.strptime(due, '%Y-%m-%d'))
+        
         db.session.add(todo)
         db.session.commit()
         return todo.json()
+    
+    def put(self):
+
+        title = request.args.get('title')
+        description = request.args.get('description')
+        due = request.args.get('due')
+
+        task = Task.query.filter_by(title=title)
+        if description:
+            task.description = description.replace("_", " ")
+        if due:
+            task.due = datetime.strptime(due, '%Y-%m-%d')
+
+        db.session.commit()
+
+        return task.json()
 
     #@jwt_required()
-    def delete(self, title):
+    def delete(self):
+        title = request.args.get('title')
         todo = Task.query.filter_by(title=title).first()
         if todo:
             db.session.delete(todo)
@@ -66,7 +101,7 @@ class AllTasks(Resource):
         todoList = Task.query.all()
         return [todo.json() for todo in todoList]
 
-api.add_resource(TodoList, f'/task/<string:title>')
+api.add_resource(TodoList, f'/task')
 api.add_resource(AllTasks, f'/tasks')
 
 if __name__=='__main__':
